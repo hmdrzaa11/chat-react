44b62d3234defb805ebdd81b05aa9b0748fe0930
Hamid Reza <hmdrzaa11@gmail.com>
2021-02-22 01:18:02 +0330

    added a loader

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./Messages.css";
 import Message from "../Message/Message";
 import ReactScroll from "react-scroll-to-bottom";
+import Loader from "../Loader/Loader";
 
 export default function Messages({ messages, name }) {
   let renderMessages = () => {
-    if (messages) {
+    if (messages.length) {
       return messages.map((message, i) => (
         <div key={i}>
           <Message message={message} name={name} />
         </div>
       ));
+    } else {
+      return (
+        <div className="Messages-loader-wrapper">
+          <Loader />
+        </div>
+      );
     }
   };
 
